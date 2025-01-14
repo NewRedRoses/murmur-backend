e0e9f3bdc84ce16ea5f436eebf96780d65041872
Mark A. <mark001616@gmail.com>
2025-01-13 22:31:28 -0500

    Add login logic on frontend post

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,17 @@ const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 passport.use(
   new localStrategy(async (username, password, done) => {
     try {
-      // db query to get user name
+      const user = await prisma.user.findFirst({
+        where: {
+          username,
+        },
+      });
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
@@ -21,14 +28,46 @@ passport.use(
   }),
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 function loginPost(req, res, next) {
+  console.log(res.get("origin"));
   passport.authenticate("local", {
-    successRedirect: "/",
+    successRedirect: "/home",
     failureRedirect: "/",
   })(req, res, next);
 }
-function signupPost(req, res) {
-  res.send("WIP - signup");
+async function signupPost(req, res) {
+  try {
+    const { fullname, username, password } = req.body;
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      await prisma.user.create({
+        data: {
+          name: fullname,
+          username: username,
+          password: hashedPassword,
+        },
+      });
+    });
+    res.end();
+  } catch (err) {
+    console.log(err);
+  }
 }
 function logoutGet(req, res) {
   res.send("WIP - logout");
