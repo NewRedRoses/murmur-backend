729b9c9123e7a05471a4b72a7bdc602d3c387592
Mark A. <mark001616@gmail.com>
2025-01-30 21:28:31 -0500

    Add ability to send & receive messages

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -9,17 +9,19 @@ function getConversations(req, res) {
       res.sendStatus(401);
     } else {
       try {
-        const conversationUserIds = await prisma.messages.groupBy({
-          by: ["senderId"],
-          where: {
-            receiverId: authData.id,
-          },
-        });
+        const conversationUserIds =
+          await prisma.$queryRaw`SELECT DISTINCT user_id
+FROM (
+    SELECT "senderId" AS user_id FROM "Messages" WHERE "receiverId" = ${authData.id} 
+    UNION
+    SELECT "receiverId" AS user_id FROM "Messages" WHERE "senderId" = ${authData.id} 
+) AS unique_users`;
+
         const conversations = await Promise.all(
           conversationUserIds.map(async (conversation) => {
             return await prisma.user.findFirst({
               where: {
-                id: conversation.senderId,
+                id: conversation.user_id,
               },
               select: {
                 id: true,
@@ -38,14 +40,91 @@ function getConversations(req, res) {
   });
 }
 
-function getMessages(req, res) {
+function getUserMessages(req, res) {
   jwt.verify(req.token, process.env.SECRET, async (error, authData) => {
     if (error) {
-      res.sendStatus(401);
+      res.sendStatus(403).end();
     } else {
-      res.json({ message: "WIP" });
+      // Works as a boolean check as well
+      const senderUserData = await prisma.user.findFirst({
+        where: {
+          username: req.params.username,
+        },
+        select: {
+          id: true,
+          username: true,
+          name: true,
+          status: true,
+        },
+      });
+
+      if (senderUserData != undefined) {
+        const messages = await prisma.messages.findMany({
+          where: {
+            OR: [
+              {
+                AND: [
+                  {
+                    senderId: {
+                      equals: senderUserData.id,
+                    },
+                  },
+                  {
+                    receiverId: {
+                      equals: authData.id,
+                    },
+                  },
+                ],
+              },
+
+              // Below here is where condition 2. will be handled
+              {
+                AND: [
+                  {
+                    senderId: {
+                      equals: authData.id,
+                    },
+                  },
+                  {
+                    receiverId: {
+                      equals: senderUserData.id,
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        });
+        res.json({ senderUserData, loggedInUserId: authData.id, messages });
+      } else {
+        console.log("user not found");
+        res.send("not found");
+      }
+    }
+  });
+}
+function postUserMessage(req, res) {
+  jwt.verify(req.token, process.env.SECRET, async (error, authData) => {
+    try {
+      if (error) {
+        return res.sendStatus(403);
+      } else {
+        console.log(req.body);
+        const { msgReceiverId, msgSenderId, message } = req.body;
+        await prisma.messages.create({
+          data: {
+            receiverId: parseInt(msgReceiverId),
+            senderId: parseInt(msgSenderId),
+            content: message,
+          },
+        });
+
+        res.json({ msg: "success send user a message" });
+      }
+    } catch (err) {
+      console.log(err);
     }
   });
 }
 
-module.exports = { getMessages, getConversations };
+module.exports = { getUserMessages, getConversations, postUserMessage };
--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -5,9 +5,12 @@ const messagesRouter = Router();
 
 const {
   getConversations,
-  getMessages,
+  getUserMessages,
+  postUserMessage,
 } = require("../controllers/messagesController.js");
 
 messagesRouter.get("/", verifyToken, getConversations);
+messagesRouter.get("/chat/:username", verifyToken, getUserMessages);
+messagesRouter.post("/chat/:username", verifyToken, postUserMessage);
 
 module.exports = messagesRouter;
